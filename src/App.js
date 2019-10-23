f471dc737815a7855cc6fc71b6f14db477a9667c
Peter Garrow <garrow.peter@gmail.com>
2019-10-22 21:40:45 -0600

    Add mines to the grid

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import './App.css';
-import Block from './components/Block'
+import React from "react";
+import "./App.css";
+import Block from "./components/Block";
 
 function App() {
-
-  const blocks = [];
-  for (let i = 0; i < 500 / 20; i++) {
-    blocks[i] = [];
-    for (let j = 0; j < 500 / 20; j++) {
-      blocks[i].push(<Block key={j} />);
+	const blocks = [];
+	let mines = 15;
+	for (let i = 0; i < 500 / 50; i++) {
+		blocks[i] = [];
+		for (let j = 0; j < 500 / 50; j++) {
+			// let mine = false;
+			// if (mines > 0 && Math.floor(Math.random() * 2)) {
+			// 	mine = true;
+			// 	mines--;
+			// }
+			blocks[i].push({
+				key: j,
+				mine: false
+			});
+		}
     }
-  }
+    
+    for (let i = 0; i < mines; i++) {
+        let row = Math.floor(Math.random() * (500 / 50));
+        let col = Math.floor(Math.random() * (500 / 50));
+        blocks[row][col].mine = true;
+    }
 
-  return (
-    <div className="App">
-      <h1>Minesweeper</h1>
-      <div
-        style={{
-          width: `500px`,
-          height: `500px`,
-          border: `1px solid black`,
-          margin: `0 auto`
-        }}
-      >
-        {blocks.map((row, index) => (
-          <div key={index} style={{ width: `100%`, lineHeight: 0}}>
-            {row}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Minesweeper</h1>
+			<div
+				style={{
+					width: `500px`,
+					height: `500px`,
+					border: `1px solid black`,
+					margin: `0 auto`
+				}}
+			>
+				{blocks.map((row, index) => (
+					<div key={index} style={{ width: `100%`, lineHeight: 0 }}>
+						{row.map((block) => <Block {...block} />)}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Block.js
+++ src/components/Block.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 
-export default () => {
+export default ({ mine }) => {
 
     const [clicked, setClicked] = useState(false);
+    const [shiftClicked, setShiftClicked] = useState(false);
+    const [backgroundColor, setBackgroundColor] = useState(`#fff`);
+
+    const handleClick = e => {
+        if (e.shiftKey) {
+            setShiftClicked(!shiftClicked);
+            setClicked(false);
+            setBackgroundColor(`rebeccapurple`);
+        } else {
+            setClicked(!clicked);
+            setShiftClicked(false);
+            setBackgroundColor(`lightblue`);
+        }
+    }
 
     return (
         <div
             style={{
-                height: `20px`,
-                width: `20px`,
+                height: `50px`,
+                width: `50px`,
                 border: `1px solid black`,
                 display: `inline-block`,
                 boxSizing: `border-box`,
-                background: clicked ? `rebeccapurple` : `#fff`,
+                background: mine ? `red` : backgroundColor,
                 cursor: `pointer`
             }}
 
-            onClick={() => setClicked(!clicked) }
+            onClick={handleClick}
         />
     )
 }
